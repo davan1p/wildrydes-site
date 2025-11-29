ad83b58245f5830d38f0fb80b4f071972d049ffc
davan1p <davan1p@cmich.edu>
2025-11-29 19:40:52 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_xWAD0VsIA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '600cn4j5l0nlcna8npshagavue', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '37dnuqfkfgor8gr2esg7mul01e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
